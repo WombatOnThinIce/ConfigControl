02270f0343e1aa7a37e1d1492c8391521423ed81
WombatOnThinIce <git@mail.snowgum.no>
2026-08-07 19:22:44 +0200

    Draft of models complete. Working on main

--- main.py
+++ main.py
@@ -1,31 +1,86 @@
 from constants import *
-from models import Project
+from models import *
 
 
 def main():
 
-    active_project: Project() | None = None
+    projects = []
+    active_project: int | None = None
     current_filepath = ""
+
+    print()
+    print(f"Welcome to ConfigControl v{VERSION}")
 
     while True:
 
-    # Print welcome and menu
-        print(f"Welcome to ConfigControl v{VERSION}")
+        # Print welcome and menu
+        print()
         if active_project == None:
-            print()
-            print("No project is loaded")
-            print("Select from the following options:")
-            print()
-            print("[1] OPEN EXISTING PROJECT")
-            print("[2] CREATE NEW PROJECT")
-            print()
-            selection = input(">> ")
+            print("No project is currently loaded")
+        else:
+            print(f"{projects[active_project -1].proj_id} - {projects[active_project -1].name} is active")
+        print("Select from the following options:")
+        print()
+        print("[1] OPEN EXISTING PROJECT")
+        print("[2] CREATE NEW PROJECT")
+        print("[3] CREATE NEW CONFIGURATION ITEM")
+        print("[4] CREATE NEW VERSION OF CONFIGURATION ITEM")
+        print("[5] CHANGE STATUS OF CONFIGURATION ITEM")
+        print("[6] CREATE PROJECT BASELINE")
+        print("[7] VIEW PROJECT SUMMARY")
+        print()
+        print("[s] SAVE CURRENT PROJECT")
+        print("[q] QUIT WITHOUT SAVING")
+        selection = input(">> ")
+
+        if selection == "1":
+            if len(projects) == 0:
+                print("No stored projects")
+                continue
+            print("Choose a project:")
+            for i, project in enumerate(projects):
+                print(f"[{i}] {project.proj_id} - {project.name}")
+            print("[b] Back to menu")
+            selection = input(">>")
+            if selection == "b":
+                continue
+            ## NEED TO COMPLETE
+
+        elif selection == "2":
+            print("CREATE A NEW PROJECT")
+            new_name = input("Enter project name >> ")
+            new_number = len(projects) + 1
+            projects.append(Project(new_name, new_number))
+            print(f"Your new project {projects[new_number -1].proj_id} - {projects[new_number -1].name} has been added")
+            print("Would you like to make it the active project? (y/n)")
+            selection = input(">>")
+            if selection == "y":
+                active_project = new_number
+            elif selection == "n":
+                continue
+            else:
+                print("Invalid entry. Project not made active")
+
+        elif selection == "3":
+            if active_project == None:
+                print("No project is active. Select a project and try again.")
+                continue
+            print(f"CREATE A NEW CONFIGURATION ITEM IN {projects[active_project -1].proj_id} - {projects[active_project -1].name}")
+            print("Select a configuration item type. Must be:")
+            for type in CI_TYPES:
+                print(f"    {type}")
+            new_type = (">>")
+            if new_type not in CI_TYPES:
+                print("Invalid type. Try again")
+                continue
+            new_name = input("Enter CI title >>")
+
+
+        elif selection == "q":
+            break
 
         else:
-            pass
-
-        if selection == "q":
-            break
+            print("Enter one of the displayed options and try again")
 
 if __name__ == "__main__":
     main()
--- models.py
+++ models.py
@@ -1,25 +1,68 @@
 import datetime
+
 from constants import *
 
-class Version():
-    def __init__(self, serial: int, creation_notes: str) -> None:
-        self.timestamp = datetime.datetime.now(tz=datetime.UTC).isoformat()
-        self.creation_notes = creation_notes
+
+def create_log_entry(notes: str) -> dict[str, str]:
+    return {"date": datetime.datetime.now(tz=datetime.UTC).isoformat(), "note": notes}
+
+class Version:
+    def __init__(self, serial: int, notes: str) -> None:
+        self.created = datetime.datetime.now(tz=datetime.UTC).isoformat()
+        self.status_last_changed = None
         self.status = STATUSES[1]
         self.version = serial
+        self.log = [create_log_entry(f"VERSION_CREATED V{self.version}>> " + notes)]
 
-        if self.status not in STATUSES:
+    def change_status(self, new_status: str, notes: str) -> None:
+        if new_status not in STATUSES:
             raise ValueError('Status type must be allowed')
+        else:
+            self.log.append(create_log_entry(f"STATUS_CHANGE - from {self.status} to {new_status}>> " + notes))
+            self.status = new_status
+            self.status_last_changed = datetime.datetime.now(tz=datetime.UTC).isoformat()
 
-class ConfigItem():
-    def __init__(self, type: str, serial: int, name: int, creation_notes: str) -> None:
-        self.timestamp = datetime.datetime.now(tz=datetime.UTC).isoformat()
-        self.creation_notes = creation_notes
+class ConfigItem:
+    def __init__(self, type: str, serial: int, name: str, ci_id: str, creation_notes: str) -> None:
+        if type not in CI_TYPES:
+            raise ValueError('CI type must be allowed')
+        else:
+            self.created = datetime.datetime.now(tz=datetime.UTC).isoformat()
+            self.version_last_added = None
+            self.creation_notes = creation_notes
+            self.name = name
+            self.serial = serial
+            self.ci_id = ci_id
+            self.type = type
+            self.log = [create_log_entry(f"CI_CREATED - type {self.type}>> " + creation_notes)]
+            self.versions = [Version(1, f"CI_CREATED - type {self.type}>> " + creation_notes)]
+
+    def add_new_version(self, notes: str) -> Version:
+        new_serial = len(self.versions) + 1
+        new_version = Version(new_serial, notes)
+        self.versions.append(new_version)
+        self.log.append(create_log_entry(f"VERSION_CREATED V{new_version}>> " + notes))
+        self.version_last_added = datetime.datetime.now(tz=datetime.UTC).isoformat()
+        return new_version
+
+class Project:
+    def __init__(self, name: str, number: int) -> None:
         self.name = name
-        self.type = type
+        self.number = number
+        self.proj_id = f"P{self.number}"
+        self.created = datetime.datetime.now(tz=datetime.UTC).isoformat()
+        self.ci_last_added = None
+        self.log = [create_log_entry(f"PROJECT_CREATED - {self.number} - {self.name}>> ")]
+        self.cis = []
 
+    def generate_ci_id(self, serial) -> str:
+        return f"{self.proj_id}-C{serial}"
 
-class Project():
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.number = issue_project_number()
+    def add_new_ci(self, type: str, name: str, notes: str) -> ConfigItem:
+        new_serial = len(self.cis) + 1
+        new_ci_id = self.generate_ci_id(new_serial)
+        new_ci = ConfigItem(type, new_serial, name, new_ci_id, notes)
+        self.cis.append(new_ci)
+        self.log.append(create_log_entry(f"CI_CREATED CI-{new_serial}>> " + notes))
+        self.ci_last_added = datetime.datetime.now(tz=datetime.UTC).isoformat()
+        return new_ci
