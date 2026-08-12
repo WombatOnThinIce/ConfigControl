853e664be2a0086535e59ce7eb451a1a5a75d506
WombatOnThinIce <git@mail.snowgum.no>
2026-08-12 11:42:56 +0200

    minor update

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class Version:
     def __init__(self, serial: int, notes: str) -> None:
         self.created = datetime.datetime.now(tz=datetime.UTC).isoformat()
         self.status_last_changed = None
-        self.status = STATUSES[1]
+        self.status = DEFAULT_STATUS
         self.version = serial
         self.log = [create_log_entry(f"VERSION_CREATED V{self.version}>> " + notes)]
 
@@ -23,25 +23,24 @@ class Version:
             self.status_last_changed = datetime.datetime.now(tz=datetime.UTC).isoformat()
 
 class ConfigItem:
-    def __init__(self, type: str, serial: int, name: str, ci_id: str, creation_notes: str) -> None:
-        if type not in CI_TYPES:
+    def __init__(self, item_type: str, serial: int, name: str, ci_id: str, creation_notes: str) -> None:
+        if item_type not in CI_TYPES:
             raise ValueError('CI type must be allowed')
-        else:
-            self.created = datetime.datetime.now(tz=datetime.UTC).isoformat()
-            self.version_last_added = None
-            self.creation_notes = creation_notes
-            self.name = name
-            self.serial = serial
-            self.ci_id = ci_id
-            self.type = type
-            self.log = [create_log_entry(f"CI_CREATED - type {self.type}>> " + creation_notes)]
-            self.versions = [Version(1, f"CI_CREATED - type {self.type}>> " + creation_notes)]
+        self.created = datetime.datetime.now(tz=datetime.UTC).isoformat()
+        self.version_last_added = None
+        self.creation_notes = creation_notes
+        self.name = name
+        self.serial = serial
+        self.ci_id = ci_id
+        self.item_type = item_type
+        self.log = [create_log_entry(f"CI_CREATED - type {self.item_type}>> " + creation_notes)]
+        self.versions = [Version(1, f"CI_CREATED - type {self.item_type}>> " + creation_notes)]
 
     def add_new_version(self, notes: str) -> Version:
         new_serial = len(self.versions) + 1
         new_version = Version(new_serial, notes)
         self.versions.append(new_version)
-        self.log.append(create_log_entry(f"VERSION_CREATED V{new_version}>> " + notes))
+        self.log.append(create_log_entry(f"VERSION_CREATED V{new_version.version}>> " + notes))
         self.version_last_added = datetime.datetime.now(tz=datetime.UTC).isoformat()
         return new_version
 
@@ -53,13 +52,15 @@ class Project:
         self.created = datetime.datetime.now(tz=datetime.UTC).isoformat()
         self.ci_last_added = None
         self.log = [create_log_entry(f"PROJECT_CREATED - {self.number} - {self.name}>> ")]
+        self.ci_counter = 0
         self.cis = []
 
     def generate_ci_id(self, serial) -> str:
         return f"{self.proj_id}-C{serial}"
 
     def add_new_ci(self, type: str, name: str, notes: str) -> ConfigItem:
-        new_serial = len(self.cis) + 1
+        self.ci_counter += 1
+        new_serial = self.ci_counter
         new_ci_id = self.generate_ci_id(new_serial)
         new_ci = ConfigItem(type, new_serial, name, new_ci_id, notes)
         self.cis.append(new_ci)
